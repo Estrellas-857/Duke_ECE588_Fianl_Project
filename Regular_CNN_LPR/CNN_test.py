96be5cc8dd3c77d2ff3fcfa4e291cfe9526ba24b
Estrellas-857 <78697964+Estrellas-857@users.noreply.github.com>
2023-12-05 11:57:20 -0500

    Update CNN_test.py

--- Regular_CNN_LPR/CNN_test.py
+++ Regular_CNN_LPR/CNN_test.py
@@ -7,24 +7,24 @@ import cv2
 ###for test only
 folder_path = 'D:/archive/test/'
 
-# 获取文件夹中的所有图片文件
+# Get all image files in the folder
 image_files = [f for f in os.listdir(folder_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
-# 构建正确答案的字典
+# Build a dictionary of correct answers
 correct_answers = {}
 for image_file in image_files:
-    # 移除文件后缀并替换'-'字符
+    # Remove file suffix and replace '-' character
     plate_number = os.path.splitext(image_file)[0].replace('-', '')
     correct_answers[image_file] = plate_number
 
 
-# 获取文件夹中的所有图片文件
+# Get all image files in the folder
 image_files = [f for f in os.listdir(folder_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
 correct_count = 0
 total_time = 0
 
-# 遍历并处理每个图像
+# Iterate through and process each image
 for image_file in image_files:
     img_path = os.path.join(folder_path, image_file)
     img = cv2.imread(img_path)
@@ -36,13 +36,13 @@ for image_file in image_files:
     total_time += time.time() - start_time
 
     correct_plate = correct_answers.get(image_file, '')
-    # 检查预测结果是否正确
+    # Check whether the prediction results are correct
     if predicted_plate == correct_plate:
         correct_count += 1
     else:
         print(f"Image: {image_file}, Predicted Plate: {predicted_plate}, Correct Plate: {correct_plate}")
 
-# 计算正确率和平均耗时
+# Calculate accuracy and average time
 accuracy = correct_count / len(image_files)
 average_time = total_time / len(image_files)
 
